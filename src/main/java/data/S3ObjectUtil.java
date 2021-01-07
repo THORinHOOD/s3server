3c7ac7e4b073141269e086b92ae2831169f6fa94
Asgar Zagitov <ihateyouthor@gmail.com>
2021-01-08 01:12:47 +0300

    added folders

--- src/main/java/data/S3ObjectUtil.java
+++ src/main/java/data/S3ObjectUtil.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Optional;
 
 public class S3ObjectUtil {
@@ -48,8 +49,15 @@ public class S3ObjectUtil {
             //TODO
             return null;
         }
-
         File file = new File(absolutePath.get());
+        if (!processFolders(file, bucket)) {
+            throw new S3Exception("Can't create folders: " + absolutePath)
+                    .setStatus(HttpResponseStatus.INTERNAL_SERVER_ERROR)
+                    .setMessage("Internal error : can't create folder")
+                    .setCode("InternalError")
+                    .setResource(File.separatorChar + bucket + key)
+                    .setRequestId("1");
+        }
         if (file.createNewFile() || file.exists()) {
             FileOutputStream outputStream = new FileOutputStream(file);
             outputStream.write(bytes);
@@ -77,6 +85,18 @@ public class S3ObjectUtil {
             return Optional.empty();
         }
         return Optional.of(basePath + File.separatorChar + bucket + key);
+    }
+
+    private static boolean processFolders(File file, String bucket) {
+        File folder = file.getParentFile();
+        if (folder.exists() && folder.isDirectory()) {
+            return true;
+        } else if (folder.exists() && !folder.isDirectory()) {
+            return false;
+        } else if (!folder.getName().equals(bucket)) {
+            return processFolders(folder, bucket) && folder.mkdir();
+        }
+        return true;
     }
 
     private static String calculateETag(byte[] bytes) {
