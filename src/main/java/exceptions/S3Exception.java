01605ee856198f87f6fc75636806db8a8927803b
Asgar Zagitov <ihateyouthor@gmail.com>
2021-01-08 02:04:28 +0300

    added xml util

--- src/main/java/exceptions/S3Exception.java
+++ src/main/java/exceptions/S3Exception.java
@@ -3,16 +3,11 @@ package exceptions;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import utils.XmlUtil;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.StringWriter;
 
 public class S3Exception extends RuntimeException {
 
@@ -38,26 +33,10 @@ public class S3Exception extends RuntimeException {
             error.appendChild(createElement(doc, "Resource", resource));
             error.appendChild(createElement(doc, "RequestId", requestId));
             doc.appendChild(error);
-            return xmlDocumentToString(doc);
+            return XmlUtil.xmlDocumentToString(doc);
         } catch (ParserConfigurationException e) {
             //TODO
             return "";
-        }
-    }
-
-    private String xmlDocumentToString(Document doc) {
-        try {
-            StringWriter sw = new StringWriter();
-            TransformerFactory tf = TransformerFactory.newInstance();
-            Transformer transformer = tf.newTransformer();
-            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-
-            transformer.transform(new DOMSource(doc), new StreamResult(sw));
-            return sw.toString();
-        } catch (Exception ex) {
-            throw new RuntimeException("Error converting to String", ex);
         }
     }
 
---
+++ src/main/java/utils/XmlUtil.java
@@ -0,0 +1,30 @@
+package utils;
+
+import org.w3c.dom.Document;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
+
+public class XmlUtil {
+
+    public static String xmlDocumentToString(Document doc) {
+        try {
+            StringWriter sw = new StringWriter();
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+
+            transformer.transform(new DOMSource(doc), new StreamResult(sw));
+            return sw.toString();
+        } catch (Exception ex) {
+            throw new RuntimeException("Error converting to String", ex);
+        }
+    }
+
+}
