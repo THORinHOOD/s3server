e7e5c97c779040ed25f8f3bf358e8900929c14ff
Asgar Zagitov <ihateyouthor@gmail.com>
2021-05-13 23:10:27 +0300

    added ListBucketsTest

---
+++ src/test/java/com/thorinhood/utils/list/ListBucketsTest.java
@@ -0,0 +1,41 @@
+package com.thorinhood.utils.list;
+
+import com.thorinhood.utils.BaseTest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+public class ListBucketsTest extends BaseTest {
+    public ListBucketsTest() {
+        super("testS3Java", 9999);
+    }
+
+    @Test
+    public void simpleListBuckets() {
+        S3Client s3Client = getS3Client(false, ROOT_USER.getAccessKey(), ROOT_USER.getSecretKey());
+        createBucketRaw(s3Client, "bucket");
+        createBucketRaw(s3Client, "bucket2");
+        S3Client s3Client2 = getS3Client(false, ROOT_USER_2.getAccessKey(), ROOT_USER_2.getSecretKey());
+        createBucketRaw(s3Client2, "bucket3");
+        ListBucketsResponse response = s3Client.listBuckets();
+        Assertions.assertEquals(2, response.buckets().size());
+        for (Bucket bucket : response.buckets()) {
+            Assertions.assertTrue(bucket.name().equals("bucket") || bucket.name().equals("bucket2"));
+        }
+        response = s3Client2.listBuckets();
+        Assertions.assertEquals(1, response.buckets().size());
+        Assertions.assertEquals("bucket3", response.buckets().get(0).name());
+        S3Client s3Client3 = getS3Client(false, NOT_AUTH_ROOT_USER.getAccessKey(),
+                NOT_AUTH_ROOT_USER.getSecretKey());
+        try {
+            s3Client3.listBuckets();
+            Assertions.fail("Must be exception");
+        } catch (S3Exception exception) {
+            Assertions.assertEquals(HttpResponseStatus.FORBIDDEN.code(), exception.statusCode());
+        }
+    }
+}
