b870283a97ec0257b44f6990b1dbe870bc3d9175
Asgar Zagitov <ihateyouthor@gmail.com>
2021-05-13 23:36:52 +0300

    added HeadObjectTest added incorrectBucketNameTest

--- src/test/java/com/thorinhood/utils/actions/CreateBucketTest.java
+++ src/test/java/com/thorinhood/utils/actions/CreateBucketTest.java
@@ -1,14 +1,18 @@
 package com.thorinhood.utils.actions;
 
 import com.thorinhood.utils.BaseTest;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.BucketAlreadyOwnedByYouException;
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
 import software.amazon.awssdk.services.s3.model.CreateBucketResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 
 import java.io.File;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CreateBucketTest extends BaseTest {
 
@@ -41,4 +45,23 @@ public class CreateBucketTest extends BaseTest {
 
     }
 
+    @Test
+    public void incorrectName() {
+        S3Client s3Client = getS3Client(false, ROOT_USER.getAccessKey(), ROOT_USER.getSecretKey());
+        incorrectNameCheck(s3Client, "aa");
+        incorrectNameCheck(s3Client, IntStream.range(0, 64).mapToObj(i -> "a").collect(Collectors.joining()));
+        incorrectNameCheck(s3Client, "isBucket?");
+        incorrectNameCheck(s3Client, ".#bucket");
+        incorrectNameCheck(s3Client, "bucket_$folder$");
+    }
+
+    private void incorrectNameCheck(S3Client s3Client, String bucket) {
+        try {
+            s3Client.createBucket(CreateBucketRequest.builder()
+                    .bucket(bucket)
+                    .build());
+            Assertions.fail("Must be exception");
+        } catch (IllegalArgumentException ignored) {
+        }
+    }
 }
---
+++ src/test/java/com/thorinhood/utils/actions/HeadObjectTest.java
@@ -0,0 +1,39 @@
+package com.thorinhood.utils.actions;
+
+import com.thorinhood.utils.BaseTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
+
+import java.util.Map;
+
+public class HeadObjectTest extends BaseTest {
+    public HeadObjectTest() {
+        super("testS3Java", 9999);
+    }
+
+    @Test
+    public void simpleHeadObject() {
+        S3Client s3Client = getS3Client(false, ROOT_USER.getAccessKey(), ROOT_USER.getSecretKey());
+        createBucketRaw(s3Client, "bucket");
+        String content = createContent(5 * 1024 * 1024);
+        String eTag = calcETag(content);
+        Map<String, String> metadata = Map.of(
+                "key1", "value1",
+                "key2", "value2"
+        );
+        putObjectRaw(s3Client, "bucket", "file.txt", content, metadata);
+        HeadObjectResponse response = s3Client.headObject(HeadObjectRequest.builder()
+                .key("file.txt")
+                .bucket("bucket")
+                .ifMatch(eTag)
+                .build());
+        Assertions.assertEquals("\"" + eTag + "\"", response.eTag());
+        Assertions.assertTrue(equalsMaps(metadata, response.metadata()));
+        Assertions.assertEquals(content.getBytes().length, response.contentLength().intValue());
+        Assertions.assertEquals("text/plain", response.contentType());
+    }
+
+}
